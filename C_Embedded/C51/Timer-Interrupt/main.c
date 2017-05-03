27471c0762a3ca62fbe71c266587d96e95fea782
xiaoyang <lixiaoyang2020@gmail.com>
2017-05-03 22:40:16 +0800

    add different main.c
    main.c with usart.c to work.

--- C_Embedded/C51/Timer-Interrupt/main.c
+++ C_Embedded/C51/Timer-Interrupt/main.c
@@ -1,71 +1,59 @@
 #include <reg52.h>
+#include "usart.h"
 
-typedef unsigned char uchar;
-typedef unsigned int uint;
-
-sbit P20=P2^0;
-sbit P21=P2^1;
-
-uchar count = 0;
-
-uchar led_code[]={
-0x3f,0x06,0x5b,0x4f,
-0x66,0x6d,0x7d,0x07,
-0x7f,0x6f};
+uchar code Led_mod[]={0x38,0x3f,0x3e,0x73};
+bit flag = 0;
+uint count;
+sbit P30 = P3^0;
+sbit P31 = P3^1;
 
 void Delay(uchar z)
 {
 	uchar a,b;
-	for(a=z;a>0;a--)
-	for(b=110;b>0;b--);	
+	for(a=z; a>0; a--)
+	for(b=110; b>0; b--);
 }
 
-void Int0_config()
+void Display_LO(uchar i)
 {
-	IT0=1;		  //set INT0 interrupt type(1: Falling 0: Low level)
-	EX0=1;		  //enable INT0 inturrupt
-	EA=1;		  //open globel inturrupt seitch
-}
+	P30 = 0;
+	P2 = Led_mod[i];
+	Delay(10);
+	P30 = 1;
 
-void led_show()
-{
-	uint x,y;
-	x = count/10;	 //十位
-	y = count%10;	 //个位
-
-	if(count <10)
-	{
-		P20 = 0;
-		P0 = led_code[y];
-	}
-	else
-	{
-		P20 = 0;
-		P0 = led_code[y];
-		Delay(10);
-		P20=1;
-
-		P21 = 0;
-		P0 = led_code[x];
-		Delay(10);
-		P21=1;
-	}
+	P31 = 0;
+	P2 = Led_mod[i+1];
+	Delay(10);
+	P31 = 1;
 }
 
 void main()
 {
-	Int0_config();
+	uint j;
+	T0_Init();
 	while(1)
 	{
-		led_show();
-	}
-}
-
-void int0() interrupt 0
-{
-	count++;
-	if(count == 100)
-	{
-		count = 0;
+		Display_LO(j);
+//		if(TF0 == 1)
+		while(TF0)
+		{
+			TH0=(65536-50000)/256;
+			TL0=(65536-50000)%256;
+			count++;
+			TF0 = 0;
+		}
+		if(count == 5)
+		{
+			count = 0;
+			flag = ~flag;
+			if(flag == 1)
+			{
+				j=2;
+			}
+			else
+			{
+				j=0;
+			}
+		}
 	}
 }
